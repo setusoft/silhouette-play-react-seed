6a1ea7f4138a5f5fbe39d69c33067b3aa42074a4
Christian Kaps <ck-github@mohiva.com>
2017-05-16 14:21:52 +0200

    Sagas are now injectable (#7)

--- app-ui/src/store/sagas.js
+++ app-ui/src/store/sagas.js
@@ -1,8 +1,17 @@
 import { combineSagas } from 'util/Saga';
 import authSaga from 'routes/Auth/sagas/AuthSaga';
+import { sagaMiddleware } from './middleware';
 
-export default function* rootSaga() {
+export function* rootSaga() {
   yield combineSagas([
     authSaga,
   ]);
 }
+
+export const injectSaga = (store, { key, saga }) => {
+  if (Object.hasOwnProperty.call(store.asyncSagas, key)) return;
+
+  const s = store;
+  s.asyncSagas[key] = saga;
+  sagaMiddleware.run(s.asyncSagas[key]);
+};
