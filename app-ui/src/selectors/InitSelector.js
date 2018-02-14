0b8af5eee44a1b95008b041227027335f7c0ea15
Christian Kaps <ck-github@mohiva.com>
2018-02-14 14:34:48 +0100

    Wrong condition for initialization status

--- app-ui/src/selectors/InitSelector.js
+++ app-ui/src/selectors/InitSelector.js
@@ -2,4 +2,4 @@
 import includes from 'lodash/includes';
 
 // eslint-disable-next-line import/prefer-default-export
-export const isInitialized = (state: Object) => !includes(state, false);
+export const isInitialized = (state: Object) => !includes(state.init, false);
